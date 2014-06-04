b39a0e89db95b983161962759da9085d723fe2b5
Ruddy <ruddy.pionneau@orange.fr>
2014-06-04 10:59:35 +0200

    tuto + sed

--- backup.sh
+++ backup.sh
@@ -8,10 +8,10 @@ tftpdir=$3
 backupdir=$4
 
 
-if [ $# -ne 4 ] 
+if [ $# -ne 4 ]
 then
      echo "Not enough arguments or too much arguments"
-     echo "Syntax: <list> <IP> <tftp directories> <backup directories>" 
+     echo "Syntax: <list> <IP> <tftp directories> <backup directories>"
      echo "Example: ./backup.sh random.list 10.10.10.20 /srv/tftp config_recup/"
      exit
 fi
@@ -29,19 +29,18 @@ do
     pass=$(echo $a | cut -f 3 -d ";")
 
     # cfg extesion is fix for SNR
-    config="$ip.cfg"
+    config="${ip}.cfg"
     echo $ip
     touch $tftpdir/$config
     chmod -R 777 $tftpdir
     chown -R nobody $tftpdir
 
     $expect dlink.exp $ip $user $pass $config $tftp && cp $tftpdir/$config $backupdir
- 
+
     rm -f $tftpdir/$config
-    
-    sed -i -e '/create/,/disable/{ /create/b;/disable/b; s/.*/******/;}' /home/ruddy/dlinkgithub/configs_tmp/172.18.78.11.cfg 
-    sed -ie 's///'    /home/ruddy/dlinkgithub/configs_tmp/172.18.78.11.cfg
-    
-    
+
+    #sed -i -e '/create/,/disable/{ /create/b;/disable/b; s/.*/******/;}' /home/ruddy/dlink-backup/config_tmp/172.18.78.11.cfg
+    #sed -ie 's///'    /home/ruddy/dlink-backup/config_tmp/172.18.78.11.cfg
+    sed -i -e 's/'${pass}'/******/g' ${backupdir}/${ip}.cfg
 done
 
