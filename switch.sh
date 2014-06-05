28b26aa436702c8432e565ca20be38dde381c49f
Ruddy Pionneau <ruddy.pionneau@orange.fr>
2014-06-05 15:27:59 +0200

    Switch parameters

--- switch.sh
+++ switch.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
+# Annotation Change IP and backup directories for your own configuration
+ip="172.18.78.90"
+dst="../config_tmp"
+
 # Fetch configurations from switches list
-./backup.sh switch.list 172.18.78.90 /srv/tftp ../config_tmp
+./backup.sh switch.list $ip /srv/tftp "$dst"
 
 # Commit new configurations and push to Gitlab
-cd ../config_tmp
+cd "$dst"
 git add *cfg
 git commit -m "Configurations `date +%Y-%m-%d\ %H:%M`"
 git push origin master
-
